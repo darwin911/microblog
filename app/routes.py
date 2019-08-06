db4a77dbffb8fd6f0ff028fc4c85ff1a996fb274
Darwin Smith <darwinpsmith@gmail.com>
2019-08-06 18:53:35 -0400

    adds posts to template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'username': 'HAL 9000'}
+    posts = [
+      {
+        'author': {'username': 'Robert'},
+        'body': 'Python and Flask are the bomb.'
+      },
+      {
+        'author': {'username': 'Dan'},
+        'body': 'What is Python? Redux is better.'
+      }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
