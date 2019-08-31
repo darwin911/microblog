b645c668b3e407917f1a7a38dbf139ed465f7742
Darwin Smith <darwinpsmith@gmail.com>
2019-08-31 15:40:37 -0400

    adds post form view/route

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.models import User
-from app.forms import RegistrationForm, LoginForm, EditProfileForm
+from app.models import User, Post
+from app.forms import RegistrationForm, LoginForm, EditProfileForm, PostForm
 from datetime import datetime
 
 @app.before_request
@@ -12,21 +12,25 @@ def before_request():
     current_user.last_seen = datetime.utcnow()
     db.session.commit()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 @login_required
 def index():
-    posts = [
-      {
-        'author': {'username': 'Robert'},
-        'body': 'Python and Flask are the bomb.'
-      },
-      {
-        'author': {'username': 'Dan'},
-        'body': 'What is Python? Redux is better.'
-      }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+  form = PostForm()
+  if form.validate_on_submit():
+    post = Post(body=form.post.data, author=current_user)
+    db.session.add(post)
+    db.session.commit()
+    flash('Your post is now live!')
+    return redirect(url_for('index'))
+  posts = [{
+      'author': {'username': 'John'},
+      'body': 'Python and Flask are the bomb.'
+      }, {
+      'author': {'username': 'Susan'},
+      'body': 'What is Python? Redux is better.'
+      }]
+  return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
